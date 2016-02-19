9e0dc7a0681fb04d250a542a98f95ba9687780f0
Brian Balsamo <balsamo@uchicago.edu>
2016-02-19 13:37:49 -0600

    more base work. experimenting with hierarchical premis field names.

--- controlledvocab/lib.py
+++ controlledvocab/lib.py
@@ -1,30 +1,102 @@
-from re import match, compile
+from re import compile, IGNORECASE
+
 
 class ControlledVocabulary(object):
-    def __init__(self, contains=None, child_vocabs=None, patterns=None):
-        if contains is None:
-            self.contains = []
-        else:
-            self.contains = contains
-        if child_vocabs is None:
-            self.child_vocabs = []
-        else:
-            for child_vocab in child_vocabs:
-                self.child_vocabs.append(child_vocab())
-        if patterns is None:
-            self.patterns = []
-        else:
-            for pattern in patterns:
-                self.patterns.append(compile(pattern))
+    def __init__(self, contains=None, child_vocabs=None, patterns=None,
+                 edit_before_build=False,
+                 check_children=True, check_patterns=True,
+                 case_sensitive=True):
 
+        self.built_contains = None
+        self.built_child_vocabs = None
+        self.built_patterns = None
+
+        self.check_children = check_children
+        self.check_patterns = check_patterns
+        self.case_sensitive = case_sensitive
+        self.edit_before_build = edit_before_build
+
+        self.set_contains(contains)
+        self.set_child_vocabs(child_vocabs)
+        self.set_patterns(patterns)
+
+        if not edit_before_build:
+            self.build_all()
 
     def __contains__(self, x):
-        if x in self.contains:
-            return True
-        for child_vocab in self.child_vocabs:
-            if x in child_vocab:
+        if self.built_contains is None or \
+                self.built_child_vocabs is None or \
+                self.built_patterns is None:
+            raise AttributeError('Can not run without built sets.')
+        if not isinstance(x, str):
+            return False
+        if self.case_sensitive:
+            if x in self.built_contains:
                 return True
-        for pattern in self.patterns:
-            if pattern.match(x):
+        else:
+            if x.lower() in self.built_contains:
                 return True
+        if self.check_children:
+            for child_vocab in self.built_child_vocabs:
+                if x in child_vocab:
+                    return True
+        if self.check_patterns:
+            for pattern in self.built_patterns:
+                if pattern.match(x):
+                    return True
         return False
+
+    def build_contains(self):
+        if self.contains is None:
+            self.built_contains = []
+        else:
+            if self.case_sensitive is False:
+                self.built_contains = [x.lower() for x in self.contains]
+            else:
+                self.built_contains = self.contains
+
+    def build_child_vocabs(self):
+        if self.child_vocabs is None:
+            self.built_child_vocabs = []
+        else:
+            self.built_child_vocabs = []
+            for child_vocab in self.child_vocabs:
+                self.built_child_vocabs.append(
+                    child_vocab(check_children=self.check_children,
+                                check_patterns=self.check_patterns,
+                                case_sensitive=self.case_sensitive,
+                                edit_before_build=self.edit_before_build)
+                )
+
+    def build_patterns(self):
+        if self.patterns is None:
+            self.built_patterns = []
+        else:
+            for pattern in self.patterns:
+                if self.case_sensitive is False:
+                    self.built_patterns.append(compile(pattern, IGNORECASE))
+                else:
+                    self.built_patterns.append(compile(pattern))
+
+    def build_all(self):
+        self.build_contains()
+        self.build_child_vocabs()
+        self.build_patterns()
+
+    def set_contains(self, contains):
+        self.contains = contains
+
+    def get_contains(self):
+        return self.contains
+
+    def set_child_vocabs(self, child_vocabs):
+        self.child_vocabs = child_vocabs
+
+    def get_child_vocabs(self):
+        return self.child_vocabs
+
+    def set_patterns(self, patterns):
+        self.patterns = patterns
+
+    def get_patterns(self):
+        return self.patterns
--- controlledvocab/premis.py
+++ controlledvocab/premis.py
@@ -1,5 +1,46 @@
-from controlledvocabulary.lib import ControlledVocabulary
+from controlledvocab.lib import ControlledVocabulary
+
 
 class PremisVocabulary(ControlledVocabulary):
-    def __init__(self):
-        ControlledVocabulary.__init__(self, child_vocabs=[ObjectVocabulary, EventVocabulary, RightsVocabulary, AgentVocabulary])
+    def __init__(self, edit_before_build=False, check_children=True,
+                 check_patterns=True, case_sensitive=True):
+        contains = ['premis']
+        child_vocabs = [EventVocabulary]
+#        child_vocabs = [ObjectVocabulary,
+#                        EventVocabulary,
+#                        RightsVocabulary,
+#                        AgentVocabulary]
+        patterns = None
+
+        ControlledVocabulary.__init__(self,
+                                      contains=contains,
+                                      child_vocabs=child_vocabs,
+                                      patterns=patterns,
+                                      edit_before_build=edit_before_build,
+                                      check_children=check_children,
+                                      check_patterns=check_patterns,
+                                      case_sensitive=case_sensitive)
+
+
+class EventVocabulary(ControlledVocabulary):
+    def __init__(self, edit_before_build=False, check_children=True,
+                 check_patterns=True, case_sensitive=True):
+        contains = ['eventType',
+                    'eventDateTime']
+
+        child_vocabs = None
+#        child_vocabs = [EventIdentifierVocabulary,
+#                        EventDetailInformationVocabulary,
+#                        EventOutcomeInformationVocabulary,
+#                        LinkingAgentIdentifierVocabulary,
+#                        LinkingObjectIdentifierVocabulary]
+        patterns = None
+
+        ControlledVocabulary.__init__(self,
+                                      contains=contains,
+                                      child_vocabs=child_vocabs,
+                                      patterns=patterns,
+                                      edit_before_build=edit_before_build,
+                                      check_children=check_children,
+                                      check_patterns=check_patterns,
+                                      case_sensitive=case_sensitive)
